cb6f3d52560b108945a2a4d981ce5579f0427820
sklizzzkiy <127947819+sklizzzkiy@users.noreply.github.com>
2023-04-17 02:21:31 +0300

    lss

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter >
+    <HashRouter >
     <Provider store={store}>
         <App/>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
 
 );
 
