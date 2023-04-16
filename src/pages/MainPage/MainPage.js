daa65f587fabd669619ad3286ff8aea0c6da54c7
sklizzzkiy <127947819+sklizzzkiy@users.noreply.github.com>
2023-04-16 17:20:42 +0300

    first commit

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainPage.css';
+
+const MainPage = () => {
+    return (
+        <div className="main-page">
+            
+        </div>
+    );
+};
+
+export default MainPage;
